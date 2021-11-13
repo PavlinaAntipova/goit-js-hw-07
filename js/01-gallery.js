24686c9cb702bf6e0f424c77865eb307ffcc4eb3
Pavlina Antipova <pavlina.antipova@gmail.com>
2021-11-13 18:29:36 +0200

    Добавляет скрипты

---
+++ js/01-gallery.js
@@ -0,0 +1,46 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+
+const galleryContainerRef = document.querySelector(".gallery");
+
+const newElements = galleryItems.map(({ preview, original, description }) => `<a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/><</a>`).join(' ');
+
+galleryContainerRef.innerHTML = newElements;
+
+let modal;
+
+const onOpenModal = event => {
+    event.preventDefault();
+
+    const originalHref = event.target.dataset.source;
+
+    modal = basicLightbox.create(`<img class="gallery__image" src="${originalHref}"/>`);
+   
+    modal.show();
+
+    galleryContainerRef.addEventListener("keydown", onCloseModalByEsc);
+}
+
+const onCloseModalByEsc = event => {
+    if (event.code === "Escape") {
+        modal.close();
+         galleryContainerRef.removeEventListener("keydown", onCloseModalByEsc);
+    }
+}
+
+galleryContainerRef.addEventListener("click", onOpenModal);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/02-lightbox.js
@@ -0,0 +1,14 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const timeCaption = 250;
+const galleryContainerRef = document.querySelector(".gallery");
+
+const newElements = galleryItems.map(({ preview, original, description }) => `<a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}"/><</a>`).join(' ');
+
+galleryContainerRef.innerHTML = newElements;
+
+const modal = new SimpleLightbox('.gallery a');
+
+modal.defaultOptions.captionsData = "alt";
+modal.defaultOptions.captionDelay = timeCaption;
